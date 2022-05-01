910ab2a8a2ea7f9dfc6a4cde0a08183503aa2a11
nevesgil <gilmarneves@outlook.com>
2022-05-01 20:45:59 -0300

    github api testing

--- working_with_apis/githubapi.py
+++ working_with_apis/githubapi.py
@@ -1,1 +1,15 @@
-address = "https://api.github.com/search/repositoes?q=language:python&sort=stars"
+import requests
+
+uri = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+
+headers = {"Accepted": "application/vnd.github.v3+json"}
+
+r = requests.get(uri, headers=headers)
+
+print(f"status code: {r.status_code}")
+
+response_dict = r.json()
+
+#
+
+print(f"total items: {response_dict['total_count']}")
