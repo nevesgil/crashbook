40edfb59d213340e07f8a8a7b0d0c0fb0589dc23
nevesgil <gilmarneves@outlook.com>
2022-05-01 14:37:05 -0300

    first test

--- testing/first_test.py
+++ testing/first_test.py
@@ -2,6 +2,8 @@ import unittest
 from function_to_test import get_formatted_name
 
 """
+# piece of code for manual testing
+
 while True:
     first = input("first name: ")
     last = input("last name: ")
@@ -17,8 +19,12 @@ while True:
 
 class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
-        formatted_name = get_formatted_name("q", "q")
+        formatted_name = get_formatted_name("janis", "joplin")
         self.assertEqual(formatted_name, "Janis Joplin")
+
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_name("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
 
 
 if __name__ == "__main__":
